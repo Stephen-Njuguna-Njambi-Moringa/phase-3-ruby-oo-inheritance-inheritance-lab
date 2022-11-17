e215fecef2701e881e6c3b4e6c05cf03da843e6e
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-11-17 12:46:58 +0300

    Completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
 
+
+
+class Student <User
+    def initialize
+        @knowledge=[]
+    end
+
+    def learn kn_string
+        @knowledge << kn_string
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
